ba2e5486831b6a46014faae4ff92b1a9e69499a8
Sven H <semiro@gmx.net>
2022-02-16 19:38:20 +0100

    Update logger.py

--- BridgeEmulator/logManager/logger.py
+++ BridgeEmulator/logManager/logger.py
@@ -7,7 +7,7 @@ def _get_log_format():
 
 class Logger:
     loggers = {}
-    logLevel = logging.DEBUG # Capture all logs prior to switching
+    logLevel = logging.WARNING # Capture all logs prior to switching
 
     def configure_logger(self, level):
         self.logLevel = getattr(logging, level)
